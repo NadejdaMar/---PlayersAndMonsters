6f1c27debc401680312009f6a9f8aeaa1fa0d1be
User <User@DESKTOP-7DEACCV>
2023-01-30 09:42:23 +0200

    Add project files.

---
+++ Наследяване- PlayersAndMonsters/Hero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string username;
+        private int level;
+        public string Username
+        {
+            get { return this.username; }
+            set { this.username = value; }
+        }
+        public int Level
+        {
+            get { return this.level; }
+            set { this.level = value; }
+        }
+        public Hero(string name,int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} " +
+                $"Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
